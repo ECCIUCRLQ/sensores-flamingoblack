1232a8ea8ebb83067d8b85b328653b3d35977821
Filip Sobejko Bogusz <filip.sobejko@gmail.com>
2019-09-15 13:44:56 -0600

    El arhcivo de cliente de FlamingoBlack fue actualizado para recibir e interpretar datos por parte del proceso encargado de recibir datos del sensor. Además, se modificó un poco la estrctura de la clase, se agregaron métodos para simplificar sintaxis y se mejoró la implementación del timeout.

--- src/cliente.py
+++ src/cliente.py
@@ -4,52 +4,81 @@ import struct
 import random
 import time
 import select
+from ipcqueue import sysvmq as SYSV
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 10001
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.settimeout(5)
+buzon = SYSV.Queue(63)
+
+class myPackage:
+
+	random_id = 0
+	date = 0
+	sensor_id = bytearray([0x02, 0x00, 0x00, 0x00])
+	sensor_type = 0x00
+	data = 0
+
+my_pack = myPackage()
+
+def packageConstructor():
+
+	sending_package = struct.pack('BIBBBBBf', my_pack.random_id, my_pack.date, my_pack.sensor_id[0], my_pack.sensor_id[1], my_pack.sensor_id[2], my_pack.sensor_id[3], my_pack.sensor_type, my_pack.data)
+	return sending_package
 
 def randomGenerator():
 
 	my_random = random.randint(0, 255)
 	return my_random
 
-class protocoloComun:
+def obtainDateData():
 
-	random_id = randomGenerator()
-	date = int(time.time())
-	sensor_id = bytearray([0x01, 0x00, 0x00, 0x02])
-	sensor_type = 0x01
-	data = 1.4878
+	sensor_data = buzon.get()
 
-my_protocol = protocoloComun()
-sending_package = struct.pack('BIBBBBBf', my_protocol.random_id, my_protocol.date, my_protocol.sensor_id[0],
-				my_protocol.sensor_id[1], my_protocol.sensor_id[2], my_protocol.sensor_id[3],
-				my_protocol.sensor_type, my_protocol.data)
+	if sensor_data[1] == 0:
+		my_pack.data = sensor_data[0]
+		my_pack.date = sensor_data[2]
+		my_pack.sensor_id[3] = 0x01
+		my_pack.sensor_type = 0x01
+		my_pack.random_id = randomGenerator()
+	elif sensor_data[1] == 1:
+		my_pack.data = sensor_data[0]
+		my_pack.date = sensor_data[2]
+		my_pack.sensor_id[3] = 0x02
+		my_pack.sensor_type = 0x03
+		my_pack.random_id = randomGenerator()
 
-timeouts = 0
+def checkResponse(data_packed):
+
+	data_unpacked = struct.unpack('BBBBB', data_packed)
+
+	if data_unpacked[0] == my_pack.random_id:
+		print("-"*30)
+		print("Package was sent and received correctly")
+		print("-"*30)
+		return 0
+	else:
+		print("-"*30)
+		print("Package was lost")
+		print("-"*30)
+		return 1
 
 while True:
-	sock.sendto(sending_package, (UDP_IP, UDP_PORT))
-	timeout = select.select([sock], [], [], 5)
-	if timeouts == 3:
-		print("Server not responding.")
-		exit()
-	elif timeout[0]:
-		data_packed, address = sock.recvfrom(1024)
-		data_unpacked = struct.unpack('BBBBB', data_packed)
-		break
-	else:
-		timeouts += 1
-		print("Timeout reached. Sending package again.")
 
-if data_unpacked[0] == my_protocol.random_id:
-	print("-"*30)
-	print("Packet was sent and received correctly")
-	print("-"*30)
-else:
-	print("-"*30)
-	print("Packet was lost")
-	print("-"*30)
+	obtainDateData()
+	sending_package = packageConstructor()
+
+	while True:
+		sock.sendto(sending_package, (UDP_IP, UDP_PORT))
+		timeout = select.select([sock], [], [], 5)
+
+		if timeout[0]:
+			data_packed, address = sock.recvfrom(1024)
+			package_state = checkResponse(data_packed)
+
+			if package_state == 0:
+				break
+		else:
+			print("Timeout reached. Sending package again.")
