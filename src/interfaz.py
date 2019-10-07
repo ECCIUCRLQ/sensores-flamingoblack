98d6b71f5725c819976411f431d0e77f7698e1f3
Filip Sobejko Bogusz <filip.sobejko@gmail.com>
2019-10-06 18:26:26 -0600

    Se empezó a desarollar el main del programa, el cual va a esperar datos del interpretador. Esos datos luego los va evaluar, va a actualizar las estructuras de datos asociadas y posteriormente se implementará el llamado al MM para guardar los datos. La tabla de páginas se arregló y se mejoró en como guarda las páginas de los sensores.

--- src/interfaz.py
+++ src/interfaz.py
@@ -1,27 +1,47 @@
 # coding=utf-8
 import os
+import time
+from ipcqueue import sysvmq as SYSV
+
+my_queue = SYSV.Queue(14)
+first_time = False
+
+def obtener_datos():
+
+    try:
+        data = my_queue.get_nowait()
+        return data
+    except:
+        print("No hay datos")
+        return 0
 
 def transform_into_key(byte_array):
 
     my_key = str(byte_array[0]) + str(byte_array[1]) + str(byte_array[2]) + str(byte_array[3])
     return my_key
 
-def update_sensor(key):
+def update_sensor(key, page):
 
     data = sensor_manager[key]
     if(data[0] == False):
         data[0] = True
-        data[1] = 5
+        data[1] = 5 # este numero es para prueba
         # data[1] = metodo de MM que devuelve numero pagina
         data[2] = 0
-        return
+        if(page == 0):
+            update_page_table(0, key)
+        else:
+            update_page_table(1, key)
     else:
         if(data[3] == 5):
             if(data[2] == 500):
                 # llamo a metodo para pedir nueva pagina
                 # data[1] = actualizo nuevo numero de pagina
                 data[2] = 5
-                return
+                if(page == 0):
+                    update_page_table(0, key)
+                else:
+                    update_page_table(1, key)
             else:
                 data[2] += 5
         else:
@@ -29,18 +49,25 @@ def update_sensor(key):
                 # llamo a metodo para pedir nueva pagina
                 # data[1] = actualizo nuevo numero de pagina
                 data[2] = 8
-                return
+                if(page == 0):
+                    update_page_table(0, key)
+                else:
+                    update_page_table(1, key)
             else:
                 data[2] += 8
 
-def update_page_table(first_time):
+
+def update_page_table(update_page, this_page):
     
-    if(first_time):
+    if(update_page):
         page_file = open("PageTableIndex.csv", "w+")
 
         for key in sensor_manager.keys():
             data = sensor_manager[key]
-            page_file.write(key + "," + str(data[1]) + "\n")
+            if(data[1] != -1):
+                page_file.write(key + "," + str(data[1]) + "\n")
+            else:
+                page_file.write(key + "\n")
 
         page_file.close()
     else:
@@ -57,11 +84,47 @@ def update_page_table(first_time):
 
         new_page_file = open("PageTableIndex.csv", "w+")
 
-        for key in sensor_manager.keys():
-            data = sensor_manager[key]
-            new_page_file.write(store_buffer.pop() + "," + str(data[1]) + "\n")
+        for line in store_buffer:
+            if(line.find(this_page) != -1):
+                data = sensor_manager[this_page]
+                new_page_file.write(line + "," + str(data[1]) + "\n")
+            else:
+                new_page_file.write(line + "\n")
 
         new_page_file.close()
+
+def main():
+
+    global first_time
+
+    try:
+        while True:
+            
+            data = obtener_datos()
+            
+            if(data != 0):
+                sensor_id = bytearray([data[0], data[1], data[2], data[3]])
+                sensor_key = transform_into_key(sensor_id)
+
+                if(first_time == False):
+                    first_time = True
+                    update_sensor(sensor_key, 1)
+                else:
+                    update_sensor(sensor_key, 0)
+
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+	    print ("\nInterface shutdown.")
+    
+    '''
+    miArreglo = bytearray([4,0,0,1])
+    new_key = transform_into_key(miArreglo)
+    update_sensor(new_key)
+    update_page_table(1)
+    time.sleep(5)
+    update_page_table(0)
+    '''
 
 sensor_manager = {
     # key: [primera vez, página actual, por donde va en la página, tamaño del dato del sensor]
@@ -79,18 +142,4 @@ sensor_manager = {
     "6002": [False, -1, -1, 8]
 }
 
-miArreglo = bytearray([4,0,0,1])
-new_key = transform_into_key(miArreglo)
-update_sensor(new_key)
-update_page_table(0)
-
-'''
-
-for key in sensor_manager.keys():
-    print key
-
-for i in sensor_manager:
-    if(i == new_key):
-        print ("Got key")
-
-'''
+main()
