82c3161a42d0819ebf22019e8352fe92039a6037
Marco Mora <54404955+MarcoMora85@users.noreply.github.com>
2019-10-22 22:43:15 -0600

    Administrador de memoria terminado
    Se termino de implemetar el administrador de memoria que sera utilizado como una clase en la Interfaz, las funciones que se van a utilizar estan documentadas con que hace cada parametro y que retorna. Las demas funciones se utilizan para el correcto funcionamiento de estas explicadas anteriormente.

--- src/administradorMem.py
+++ src/administradorMem.py
@@ -1,12 +1,13 @@
 import os
+import struct
 
 class AdministradorMem:
 
     def __init__(self):
         self.memoriaPrincipal = [bytearray(200000),bytearray(2500)]
 
-        self.contadorPaginas = 0
-        self.tablaPaginas = []
+        self.contadorPaginas = self.cargarContPaginas()
+        self.tablaPaginas = self.cargarTablaPaginas()
         self.colaPaginas = [[],[]]
 
     def pagEstaEnPrincipal(self,numPagina):
@@ -26,16 +27,25 @@ class AdministradorMem:
     def getPaginaPorCambiar(self, tamano):
         return self.colaPaginas[tamano].pop()
 
-    
-    def vaciarPagina(self,tamano,posicion):
-        longitud = 0
-        if (tamano == 0):
-            longitud = 40000
-        else:
-            longitud == 500
+    def cargarContPaginas(self):
+        contador = 0
+        ultimaPagEncontrada = False
+        while not ultimaPagEncontrada:
+            try:
+                archivo = open("paginas/" + str(contador) + ".txt","r")
+                contador+=1
+            except:
+                ultimaPagEncontrada = True
 
-        for i in range (posicion,posicion+longitud):
-            self.memoriaPrincipal[tamano][i] == 0x00
+        print(contador)
+        return contador
+
+    def cargarTablaPaginas(self):
+        tabla = []
+        for i in range(self.contadorPaginas):
+            tabla.append(-1)
+
+        return tabla
 
     def enviarPagMemSecundaria(self, tamano, numPagina):
         busDatos = bytearray(1)
@@ -51,9 +61,9 @@ class AdministradorMem:
         archivo = open("paginas/" + str(numPagina) + ".txt","w+b")
 
         archivo.write(busDatos)
-        
-        self.vaciarPagina(tamano,posicion)
         self.tablaPaginas[numPagina] = -1
+        self.vaciarPagina(tamano,posicion)
+        archivo.close()
 
     def cargarPagMemSecundaria(self,tamano,numPagina,posicion):
         nombreArchivo = "paginas/" + str(numPagina) + ".txt"
@@ -69,8 +79,19 @@ class AdministradorMem:
 
         for i in range(posicion,posicion+longitud):
             self.memoriaPrincipal[tamano][i] = busDatos[i-posicion]
-            
-        self.tablaPaginas[numPagina] = posicion
+
+        self.tablaPaginas[numPagina]= posicion
+        archivo.close()
+
+    def vaciarPagina(self,tamano,posicion):
+        longitud = 0
+        if (tamano == 0):
+            longitud = 40000
+        else:
+            longitud == 500
+
+        for i in range (posicion,posicion+longitud):
+            self.memoriaPrincipal[tamano][i] == 0x00
 
     def buscarPosicionVacia(self,tamano):
         posicion = 0
@@ -99,14 +120,44 @@ class AdministradorMem:
 
         return posicion
 
+    def leerUnDato(self,tamano,posicion,tamanoDato,formato):
+        datoBytes = bytearray(tamanoDato)
+        for k in range (tamanoDato):
+            datoBytes[k] = self.memoriaPrincipal[tamano][posicion+k]
+
+        datoReal = 0
+        if(tamano == 0):
+            if(formato==0):
+                datoReal = struct.unpack('Ii', datoBytes)
+            else:
+                datoReal = struct.unpack('If', datoBytes)
+        else:
+            datoReal = struct.unpack('IB', datoBytes)
+
+        return datoReal[0], datoReal[1]
+
     """
-    Funciones a utilizar en interfaz
+    --------------------------------------------------------------------------------------------------------
+    Funciones a utilizar en Interfaz
+    --------------------------------------------------------------------------------------------------------
     """
-    
+
+    """
+    Funcion que guarda un dato dado por la interfaz.
+    Parametros:
+        -tamano: El tamano de la pagina donde se va a guardar el dato.
+            tamano = 0 : Paginas de 40KB
+            tamano = 1 : Paginas de 0.5KB
+        -numPagina: Numero de pagina donde se quiere guardar el dato.
+        -offset: Corrimiento a partir del inicio de la pagina seleccionada
+                 donde se quiere guardar el dato.
+        -dato: Dato a guarda en la recibido en forma de bytes o bytearray.
+    """
     def guardarDato(self,tamano,numPagina,offset,dato):
         posicion = -1
         if (self.pagEstaEnPrincipal(numPagina)):
             posicion = self.tablaPaginas[numPagina]
+
         else:
             posicion = self.swapPaginas(tamano,numPagina)
 
@@ -120,13 +171,21 @@ class AdministradorMem:
         for i in range (posicion,posicion+desplaz):
             self.memoriaPrincipal[tamano][i] = dato[i-posicion]
 
-
+    """
+    Funcion reserva una pagina en memoria y devuelve el numero de pagina.
+    Parametros:
+        -tamano: Tamano de la pagina que se quiere reservar.
+            tamano = 0 : Paginas de 40KB
+            tamano = 1 : Paginas de 0.5KB
+    Retorno:
+        -pagAsignada: Numero de pagina quue fue asignada con el llamado a
+                      la funcion.
+    """
     def reservarPagina(self,tamano):
         posicion = 0
         if self.getMemPrinLlena(tamano):
             paginaCambiar = self.getPaginaPorCambiar(tamano)
             posicion = self.tablaPaginas[paginaCambiar]
-            self.tablaPaginas[paginaCambiar] = -1
             self.enviarPagMemSecundaria(tamano,paginaCambiar)
         else:
             posicion = self.buscarPosicionVacia(tamano)
@@ -136,53 +195,75 @@ class AdministradorMem:
         pagAsignada = self.contadorPaginas
         self.contadorPaginas+=1
 
-        print(self.tablaPaginas)
-        print(self.colaPaginas)
-        print("-----------")
-
         return pagAsignada
 
-    def obtenerDatos(self,tamano,listaPaginas):
+
+    """
+    Funcion que devuelve todos los datos de todas las paginas pedidas hasta
+    el offset de la ultima pagina en la lista.
+    Parametros:
+        -tamano: Tamano de las paginas a consultar.
+            tamano = 0 : Paginas de 40KB
+            tamano = 1 : Paginas de 0.5KB
+        -listaPaginas: Lista de todas las paginas que se quieren consultar.
+        -offsetUltPag: offset de la ultima pagina en la lista para evitar copiar
+                       todos los ceros que puede tener la ultima pagina si esta
+                       medio llena.
+                       >Si tamano = 0: offset <= 40000 y multiplos de 8.
+                       >Si tamano = 1: offset <= 500 y multiplos de 5.
+        -formato8Bytes: Indicador del tipo de datos que se devuelve de las paginas
+                        de 40KB. Este no tiene ningun efecto cuando las paginas
+                        son de 0.5KB.
+            formato8Bytes = 0 : Datos enteros
+            formato8Bytes = 1 : Datos de punto flotante.
+    Retorno:
+        -busDatos: Lista con todos los datos solicitados de la forma
+                   fecha-dato-fecha-dato...
+    """
+    def obtenerDatos(self,tamano,listaPaginas,offsetUltPag,formato8Bytes):
         tamanoPagina = 0
+        tamanoDato = 0
         if (tamano == 0):
             tamanoPagina = 40000
+            tamanoDato = 8
         else:
             tamanoPagina = 500
+            tamanoDato = 5
 
-        tamanoBus = tamanoPagina*len(listaPaginas)
+        busDatos = []
 
-        busDatos = bytearray(tamanoBus)
-
-        contadorBus = 0
-        for i in range (len(listaPaginas)):
+        for i in range (len(listaPaginas)-1):
             if self.pagEstaEnPrincipal(listaPaginas[i]):
                 posicion = self.tablaPaginas[listaPaginas[i]]
             else:
                 posicion = self.swapPaginas(tamano,listaPaginas[i])
 
-            for j in range (posicion,posicion+tamanoPagina):
-                busDatos[contadorBus] = self.memoriaPrincipal[tamano][j]
-                contadorBus+=1
+            for j in range (posicion,posicion+tamanoPagina,tamanoDato):
+                fecha , dato = self.leerUnDato(tamano,j,tamanoDato,formato8Bytes)
+                busDatos.append(fecha)
+                busDatos.append(dato)
+
+        if self.pagEstaEnPrincipal(listaPaginas[-1]):
+            posicion = self.tablaPaginas[listaPaginas[-1]]
+        else:
+            posicion = self.swapPaginas(tamano,listaPaginas[-1])
+
+        for j in range(posicion,posicion+offsetUltPag,tamanoDato):
+            fecha , dato = self.leerUnDato(tamano,j,tamanoDato,formato8Bytes)
+            busDatos.append(fecha)
+            busDatos.append(dato)
 
         return busDatos
 
-    def printMemPrin(self):
-        print(self.memoriaPrincipal[1])
+    """
+    Funcion que guarda todas las paginas actualmente presentes en memoria principal
+    en caso que se haga un Ctrl+C desde Interfaz para proteger los datos.
+    """
+    def salvarMemPrincipal(self):
+        for i in range (len(self.tablaPaginas)):
+            if i in self.colaPaginas[0]:
+                self.enviarPagMemSecundaria(0,i)
+            elif i in self.colaPaginas[1]:
+                self.enviarPagMemSecundaria(1,i)
 
-
-
-"""
-admin = AdministradorMem()
-admin.reservarPagina(1)
-admin.reservarPagina(0)
-admin.reservarPagina(1)
-admin.reservarPagina(0)
-admin.reservarPagina(0)
-admin.reservarPagina(1)
-admin.reservarPagina(1)
-admin.reservarPagina(1)
-admin.reservarPagina(1)
-dato = bytearray([0x01,0x02,0x03,0x04,0x05])
-admin.guardarDato(1,0,0,dato)
-admin.printMemPrin()
-"""
+        print("Todas las paginas en memoria principal fueron guardadas en memoria secundaria.")
