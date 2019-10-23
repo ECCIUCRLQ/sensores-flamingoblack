71e88c7f972a4e7fafde1d26978ac1e32dad43c8
Paguva <paolaguva2495@gmail.com>
2019-10-23 17:03:03 -0600

    Recolector de datos sin sensor

---
+++ MaterialApoyo/RecolectorDatosLuzSinSensor.py
@@ -0,0 +1,35 @@
+#import RPi.GPIO as GPIO
+import socket
+import time
+import datetime
+import random
+from ipcqueue import sysvmq as SYSV
+
+#GPIO.setmode(GPIO.BCM)
+#GPIO_PIR = 4
+#GPIO.setup(GPIO_PIR,GPIO.IN)
+
+buzon = SYSV.Queue(63)
+#file = open("datos.txt", "w+")
+
+try:
+    while True:
+        valor = random.randint(0, 1)
+        if valor == 0:
+            now = time.time()
+            buzon.put([1, 2, now])
+            #file.write("Light no detected. Date " + now.strftime("%d/%m/%Y Time %Hh:%Mm:%Ss") + "\n")
+            print("Luz :) " + str(time.ctime(now))) 
+            #print(buzon.get())       
+        else:
+            now = time.time()
+            buzon.put([2, 2, now])
+            #file.write("Intruder detected. Date " + now.strftime("%d/%m/%Y Time %Hh:%Mm:%Ss") + "\n")
+            print("Keep Alive " +  str(time.ctime(now)))
+            #print(buzon.get()) 
+        time.sleep(1)
+except KeyboardInterrupt:
+    now = time.time()
+    #file.write("User exited. Date " + now.strftime("%d/%m/%Y Time %Hh:%Mm:%Ss"))
+    #file.close()
+    #GPIO.cleanup()
