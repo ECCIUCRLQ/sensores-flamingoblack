dd54b647c727bd3761d56f70f7e0552a9eb8b5ca
Filip Sobejko Bogusz <filip.sobejko@gmail.com>
2019-11-03 17:32:59 -0600

    Se agregó un método que busca alfabeticamente la ip más pequeña y establece esa como activa. Se hicieron cambios extra en el código.

--- src/interfazDistribuida.py
+++ src/interfazDistribuida.py
@@ -1,54 +1,87 @@
+# coding=utf-8
 import time
 import os
 
-# ------------------
-# Hash que mantiene los metadatos de los nodos
-# ------------------
 
-node_manager = {
+class interface:
 
-	# la hash crece conforme llegan mas nodos
-	# identificador: [ip, lista con paginas en ese nodo, espacio disponible en el nodo]
+	def __init__(self):
 
-	1: [-1, [], -1]
-	2: [-1, [], -1]
-	3: [-1, [], -1]
-	4: [-1, [], -1]
-}
+		# Hash que mantiene los metadatos de los nodos
+		self.node_manager = {
 
-# ------------------
-# Hash que crecera a medida que lleguen paginas, se asocia pagina:nodo
-# ------------------
+			# la hash crece conforme llegan mas nodos
+			# identificador: [ip, lista con paginas en ese nodo, espacio disponible en el nodo]
 
-page_ manager {
+			1: [-1, [], -1],
+			2: [-1, [], -1],
+			3: [-1, [], -1],
+			4: [-1, [], -1]
+		}
 
-}
+		# Hash que crecera a medida que lleguen paginas, se asocia pagina:nodo
+		self.page_manager = {
 
-def save_data(data_to_be_saved):
-	
-	minimum_available = 100000000
-	which_node = 0
-	
-	for key in node_manager:
+		}
+
+		self.active = False
+		self.myIp = "127.0.0.1"
+
+	# ------------------
+	# Método que buscar por best-fit cuál nodo es el más adecuado
+	# ------------------
+
+	def save_data(self, data_to_be_saved):
 		
-		node_data = node_manager[key]
+		minimum_available = 100000000
+		which_node = 0
 		
-		if minimum_available > node_data[2] and node_data[2] > len(data_to_be_saved):
+		for key in self.node_manager:
 			
-			minimum_available = node_data[2]
-			which_node = key
+			node_data = self.node_manager[key]
 			
-	# se manda al nodo which_node el dato
-	# se espera de vuelta el tamaño con el que quedo para actualizar
-	
-def recover_data(page_list):
-	
-	for page in page_list:
-		
-		for page_node in page_manager:
-			
-			if page_node == page:
+			if minimum_available > node_data[2] and node_data[2] > len(data_to_be_saved):
 				
-				# le pido al nodo que me envie esa pagina
-				# le envio esa pagina a la memoria local
-				time.sleep(5)
+				minimum_available = node_data[2]
+				which_node = key
+				
+		# se manda al nodo which_node el dato
+		# se espera de vuelta el tamaño con el que quedo para actualizar
+	
+
+	# ------------------
+	# Método que itera sobre la lista de páginas y le pide a los nodos los datos, duerme 5 segundos por cada envio al administrador local
+	# ------------------
+
+	def recover_data(self, page_list):
+		
+		for page in page_list:
+			
+			for page_node in self.page_manager:
+				
+				if page_node == page:
+					
+					# le pido al nodo que me envie esa pagina
+					# le envio esa pagina a la memoria local
+					time.sleep(5)
+
+		return 0
+
+	def set_active_interface(self, ipList):
+
+		lowestIp = ""
+
+		for ip in ipList:
+
+			if lowestIp < ipList[ip]:
+
+				lowestIp = ipList[ip]
+
+		if lowestIp == self.myIp:
+
+			self.active = True
+			# hago broadcast que soy activo
+
+		return 0
+
+print ("Todo compila")
