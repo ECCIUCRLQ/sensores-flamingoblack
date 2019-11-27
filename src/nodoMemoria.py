27fbe408f7ce46c40885f639ecf0e4de8b84d950
Polina <polina.tarassenko@ucr.ac.cr>
2019-11-26 23:08:15 -0600

    Avance NodoMem

--- src/nodoMemoria.py
+++ src/nodoMemoria.py
@@ -8,6 +8,22 @@ import manejadorPaquetes as manepack
 lock = threading.Lock()
 first_time = True
 
+class NodoMemoria():
+
+    def __init__(self):
+        self.memoria = bytearray(50008)
+        self.puerto_broad_ID = 5000
+        self.puerto_tcp_ID = 3114
+        self.hostID = "127.0.0.1"
+        self.socketID = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.node_size = 50000
+
+        registration = False
+
+        while(registration == false):
+            paquete = manepack.paquete_broadcast_estoyAqui_NM_ID(op_code, self.node_size) #agregar el op.code
+
+
 class threadsInterface(threading.Thread):
 
     def __init__(self, name):
